3a0944114757fe8d6750089f8f521bd9b06d3944
ismaelbouzrouti <132908223+ismaelbouzrouti@users.noreply.github.com>
2023-11-28 18:14:03 +0100

    creation git repo

---
+++ src/test/java/GEOActivities/start/GeoActivitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package GEOActivities.start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoActivitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
