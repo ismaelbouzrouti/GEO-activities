e8cffd19cb3412fde69896885bf21406d42465de
ismaelbouzrouti <132908223+ismaelbouzrouti@users.noreply.github.com>
2023-12-01 23:37:47 +0100

    autowired and repo annotations used

--- src/main/java/GEOActivities/start/SignupController.java
+++ src/main/java/GEOActivities/start/SignupController.java
@@ -1,6 +1,7 @@
 package GEOActivities.start;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class SignupController {
 
+    @Autowired
     private UserRepository userRepository;
 
     @GetMapping("/signup")
--- src/main/java/GEOActivities/start/UserRepository.java
+++ src/main/java/GEOActivities/start/UserRepository.java
@@ -1,9 +1,12 @@
 package GEOActivities.start;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 
     User findByEmail(String email);
+
 
 }
